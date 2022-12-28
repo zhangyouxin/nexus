daf9e0f5d360d0d36102397bdf91cc10a1213a82
zhangyouxin <zhangyouxingg@126.com>
2022-12-28 20:34:45 +0800

    add wallet key management

--- packages/extension-chrome/src/pages/Popup/Popup.tsx
+++ packages/extension-chrome/src/pages/Popup/Popup.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
-import Logo from './logo.svg';
+import { generateMnemonic } from '../../wallet/key';
 import './Popup.css';
 
 export const Popup: React.FC = () => {
+  const [mnemonic, setMnemonic] = React.useState<string>();
+  const handleCreateAccount = () => {
+    const mnemonic = generateMnemonic();
+    setMnemonic(mnemonic);
+  };
   return (
     <div className="container">
-      <img src={Logo} alt="logo" />
-      <h1>Hello Nexus</h1>
+      <button onClick={handleCreateAccount}>Create Account</button>
+      <span>{mnemonic}</span>
     </div>
   );
 };
